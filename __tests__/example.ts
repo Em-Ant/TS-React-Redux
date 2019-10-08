d78d5c5c2daf6407e87b30aac5db881cc5d2c45b
emant <joshua999x@gmail.com>
2019-10-08 22:49:51 +0200

    move tests in src

--- __tests__/example.ts
+++
@@ -1,5 +0,0 @@
-const sum = (a: number, b: number): number => a + b;
-
-test('test example', () => {
-  expect(sum(5, 4)).toEqual(9);
-});
---
+++ src/__tests__/example.ts
@@ -0,0 +1,27 @@
+import { runSaga } from 'redux-saga';
+import { put } from 'redux-saga/effects';
+
+const sum = (a: number, b: number): number => a + b;
+
+test('test example', () => {
+  expect(sum(5, 4)).toEqual(9);
+});
+const TEST = 'TEST' as const;
+const emitTest = () => ({ type: TEST });
+type TestAction = ReturnType<typeof emitTest>;
+
+test('test runsaga', () => {
+  const dispatched: TestAction[] = [];
+  const getState = () => ({ test: 'ok' });
+  const dispatch = (action: any) => dispatched.push(action);
+  runSaga(
+    {
+      dispatch,
+      getState
+    },
+    function* test() {
+      yield put(emitTest());
+    }
+  );
+  expect(dispatched).toEqual([emitTest()]);
+});
