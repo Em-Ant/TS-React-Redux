d470c3a9c5ad37c667776e4a228fe59e54ff1331
emant <joshua999x@gmail.com>
2020-01-12 19:08:41 +0100

    remove unused, update deps

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,7 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map',
-  plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    })
-  ]
+  devtool: 'source-map'
 });
