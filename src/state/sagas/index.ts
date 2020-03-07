fa4b92f431432bc828ba0e325f77b6b3a3a3ec7d
emant <joshua999x@gmail.com>
2020-03-08 00:28:04 +0100

    experoments with maybe monad

--- src/state/sagas/index.ts
+++ src/state/sagas/index.ts
@@ -1,9 +1,18 @@
 import { delay, put, all } from 'redux-saga/effects';
+import Maybe from '../../utils/Maybe';
+
+const getRandomPayload = () => {
+  const x = Math.random() > 0.1 ? Math.random() * 5 : null;
+  return Maybe.of(x)
+    .map(x => (x > 2 ? x * 2 : undefined))
+    .flatMap(x => (x < 9 ? Maybe.of(x) : Maybe.Nothing()))
+    .getOrElse('invalid');
+};
 
 function* heartBeat() {
   while (true) {
     yield delay(5000);
-    yield put({ type: 'KEEP_ALIVE' });
+    yield put({ type: 'KEEP_ALIVE', payload: getRandomPayload() });
   }
 }
 
---
+++ src/utils/Maybe.ts
@@ -0,0 +1,38 @@
+class Maybe<T> {
+  private readonly value: T;
+  private static isNonNullable<T>(value?: T | null): value is NonNullable<T> {
+    return value !== null && value !== undefined;
+  }
+  constructor(value: T) {
+    this.value = value;
+  }
+  static Nothing(): Maybe<null> {
+    return Maybe.of(null);
+  }
+  static of<T>(value: T): Maybe<T> {
+    return new Maybe<T>(value);
+  }
+  map<U>(fn: (arg: NonNullable<T>) => U): Maybe<U | null> {
+    return !Maybe.isNonNullable(this.value)
+      ? Maybe.Nothing()
+      : Maybe.of(fn(this.value));
+  }
+  flatMap<U>(fn: (arg: NonNullable<T>) => Maybe<U>): Maybe<U | null> {
+    if (!Maybe.isNonNullable(this.value)) return Maybe.Nothing();
+    return fn(this.value);
+  }
+  getOrElse<E>(elseVal: E): NonNullable<T> | E {
+    return !Maybe.isNonNullable(this.value) ? elseVal : this.value;
+  }
+  getOrEmptyArray<A = T>() {
+    return this.getOrElse([] as A[]);
+  }
+  getOrNull() {
+    return this.getOrElse(null);
+  }
+  isNothing() {
+    return !Maybe.isNonNullable(this.value);
+  }
+}
+
+export default Maybe;
