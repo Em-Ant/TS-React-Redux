dc5d0a6d689750bab5dc33cd6a155a2613abee78
emant <joshua999x@gmail.com>
2020-05-03 23:36:19 +0200

    use maybe in heplers

--- src/state/helpers.ts
+++ src/state/helpers.ts
@@ -1,4 +1,5 @@
 import { Item } from '../models';
+import Maybe from '../utils/Maybe';
 
 const symbols = 'abcdef01234567890'.split('');
 export const getUid = (n = 5): string =>
@@ -6,33 +7,20 @@ export const getUid = (n = 5): string =>
     .map((): string => symbols[Math.floor(Math.random() * 16)])
     .join('');
 
-export const getStateFromStorage = (): readonly Item[] => {
-  if (!window.localStorage) return [];
-  try {
-    return JSON.parse(window.localStorage.getItem('item_list') as string) || [];
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e);
-    return [];
-  }
-};
+export const getStateFromStorage = (): readonly Item[] =>
+  Maybe.of(window.localStorage)
+    .map((storage) => storage.getItem('item_list'))
+    .map(JSON.parse)
+    .getOrElse([]);
 
 export const saveStateToStorage = (state: Item[]): void => {
-  if (!window.localStorage) return;
-  try {
-    window.localStorage.setItem('item_list', JSON.stringify(state));
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e);
-  }
+  Maybe.of(window.localStorage).map((storage) =>
+    storage.setItem('item_list', JSON.stringify(state))
+  );
 };
 
 export const deleteStorage = (): void => {
-  if (!window.localStorage) return;
-  try {
-    window.localStorage.removeItem('item_list');
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e);
-  }
+  Maybe.of(window.localStorage).map((storage) =>
+    storage.removeItem('item_list')
+  );
 };
