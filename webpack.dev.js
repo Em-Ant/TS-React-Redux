db5975c20fe62b5e94fafc93ba4720da8e5ba83c
emant <joshua999x@gmail.com>
2020-08-16 23:04:20 +0200

    update dependencies

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,16 +1,14 @@
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-  ],
+  plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
     contentBase: './dist',
     hot: true,
-    historyApiFallback: true
-  }
+    historyApiFallback: true,
+  },
 });
