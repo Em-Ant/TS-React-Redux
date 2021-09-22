3f189640d6190133705eb988e9f21d60afa7402a
emant <joshua999x@gmail.com>
2021-09-22 19:09:21 +0200

    update deps

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,13 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const webpack = require('webpack');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
-  plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
-    contentBase: './dist',
+    static: {
+      directory: './dist',
+    },
     hot: true,
     historyApiFallback: true,
   },
